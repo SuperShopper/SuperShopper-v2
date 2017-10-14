cca632f07f71342c9e3730ce9bf2e4f267146a93
Gordon <godronus@yahoo.com>
2017-10-14 11:02:41 -0700

    merged with Katherine

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-// import homeLogo from '../../img/homeIcon.png';
-
-const homeLogo = 'src/img/homeIcon.png';
+import homeLogo from '../img/homeIcon.png';
 
 const styles = {
   width: '100%',
   height: '75px',
   backgroundColor: 'blue',
-  color: 'white'
+  color: 'white',
 }
 
 const imgStyle = {
@@ -16,20 +14,10 @@ const imgStyle = {
   width: '50px',
   position: 'absolute',
   left: '10px',
-  top: '10px'
+  top: '10px',
 }
 
 class Header extends Component {
-  
-  constructor(props) {
-    super(props);
-    console.warn('Header Loading..');
-    console.log("homeLogo", homeLogo);  
-    console.log("__dirname: ", __dirname);
-    const myHomeLogo = __dirname + 'src/img/homeIcon.png';
-    console.log("myhomeLogo", myHomeLogo);  
-  }
-
   render() {
     return (
       <div style={styles}>
